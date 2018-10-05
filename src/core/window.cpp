e9d686220a13184826fea43d9b1d57f8d098a7e3
Trevor Fisher <minalear@gmail.com>
2018-10-04 22:37:49 -0500

    Switched the window functions to be contained in a GameWindow class.

--- src/core/window.cpp
+++ src/core/window.cpp
@@ -8,63 +8,46 @@
 #include "SDL2/SDL.h"
 #include "glad/glad.h"
 
-#define PC_BUILD
+minalear::GameWindow::GameWindow(const char *title, uint32_t width, uint32_t height) {
+    // Initialize SDL and create a window
+    SDL_Init(SDL_INIT_VIDEO);
 
-#ifdef LAPTOP_BUILD
-const int OPENGL_MAJOR_VERSION = 3;
-const int OPENGL_MINOR_VERSION = 1;
-#endif
-#ifdef PC_BUILD
-const int OPENGL_MAJOR_VERSION = 4;
-const int OPENGL_MINOR_VERSION = 0;
-#endif
-
-SDL_Window* window;
-SDL_GLContext context;
-
-struct {
-    int width, height;
-} window_info;
-
-uint32_t start_time;
-
-void minalear::init_game_window(const char win_name[], const int window_width, const int window_height) {
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
-
+    // Set SDL OpenGL attributes (Core Profile, OpenGL v4.0)
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, OPENGL_MAJOR_VERSION);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, OPENGL_MINOR_VERSION);
 
-    //TODO: Vsync option doesn't seem to affect PC Workstation
-    //SDL_GL_SetSwapInterval(0);
+    // SDL_GL_SetSwapInterval(0); // VSYNC
 
-    window = SDL_CreateWindow(win_name,
-                              SDL_WINDOWPOS_CENTERED,
-                              SDL_WINDOWPOS_CENTERED,
-                              window_width, window_height,
-                              SDL_WINDOW_OPENGL);
-    window_info.width = window_width;
-    window_info.height = window_height;
+    sdl_window = SDL_CreateWindow(title,
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            width, height,
+            SDL_WINDOW_OPENGL);
+    client_width  = width;
+    client_height = height;
 
-    context = SDL_GL_CreateContext(window);
-    start_time = SDL_GetTicks();
+    gl_context = SDL_GL_CreateContext(sdl_window);
+    start_time = SDL_GetTicks(); // Used to calculate delta time for our update loop
+
+    int gladLoadSuccess = gladLoadGLLoader(SDL_GL_GetProcAddress);
+    assert(gladLoadSuccess);
 }
 
-void minalear::init_opengl() {
-    int success = gladLoadGLLoader(SDL_GL_GetProcAddress);
-    assert(success);
-}
-
-void minalear::swap_buffers() {
-    SDL_GL_SwapWindow(window);
-}
-
-void minalear::finalize_window() {
-    SDL_GL_DeleteContext(context);
+minalear::GameWindow::~GameWindow() {
+    SDL_GL_DeleteContext(gl_context);
     SDL_Quit();
 }
 
-float minalear::dt() {
+int minalear::GameWindow::width(){
+    return client_width;
+}
+int minalear::GameWindow::height() {
+    return client_height;
+}
+
+/// Call once per frame to get the delta time from the previous frame.
+float minalear::GameWindow::dt() {
     uint32_t ticks = SDL_GetTicks() - start_time;
     float dt = ticks * 0.001f;
     start_time = SDL_GetTicks();
@@ -72,9 +55,6 @@ float minalear::dt() {
     return dt;
 }
 
-int minalear::get_window_width() {
-    return window_info.width;
-}
-int minalear::get_window_height() {
-    return window_info.height;
+void minalear::GameWindow::swap_buffers() {
+    SDL_GL_SwapWindow(sdl_window);
 }
--- src/core/window.h
+++ src/core/window.h
@@ -5,15 +5,29 @@
 #ifndef MINALEAR_WINDOW_H
 #define MINALEAR_WINDOW_H
 
-namespace minalear {
-    void init_game_window(const char win_name[], int window_width, int window_height);
-    void init_opengl();
-    void swap_buffers();
-    void finalize_window();
-    float dt();
+#include "SDL2/SDL.h"
 
-    int get_window_width();
-    int get_window_height();
+namespace minalear {
+    const int OPENGL_MAJOR_VERSION = 4;
+    const int OPENGL_MINOR_VERSION = 0;
+
+    class GameWindow {
+        uint32_t client_width;
+        uint32_t client_height;
+        uint32_t start_time;
+
+        SDL_Window* sdl_window;
+        SDL_GLContext gl_context;
+
+    public:
+        GameWindow(const char* title, uint32_t width, uint32_t height);
+        ~GameWindow();
+
+        int width();
+        int height();
+        float dt();
+        void swap_buffers();
+    };
 }
 
 #endif //MINALEAR_WINDOW_H
