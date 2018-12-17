c74af08b0b6c7e117cb7e4b7561e0710b3855a39
Trevor Fisher <minalear@gmail.com>
2018-12-17 15:20:11 -0600

    Added new mingw32 path

--- cmake/FindMINGW32.cmake
+++ cmake/FindMINGW32.cmake
@@ -1,13 +1,8 @@
-find_path(MINGW32_INCLUDE
-        NAMES memory.h
-        PATHS
-        C:/MinGW/include
-        C:/MinGW/x86_64-w64-mingw32/include
-        C:/MinGW/i686-w64-mingw32/include)
 find_library(MINGW32_LIB mingw32
         PATHS
         C:/MinGW/lib
         C:/MinGW/x86_64-w64-mingw32/lib
-        C:/MinGW/i686-w64-mingw32/lib)
+        C:/MinGW/i686-w64-mingw32/lib
+        C:/msys64/mingw32/i686-w64-mingw32/lib)
 
 #include_directories(${MINGW32_INCLUDE})
