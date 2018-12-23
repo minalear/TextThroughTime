b6c4b49cb160d2ab97fda91dde00a8110331b886
Trevor Fisher <minalear@gmail.com>
2018-12-22 23:47:19 -0600

    Testing item scripting functionality.

--- scripts/init.lua
+++ scripts/init.lua
@@ -26,13 +26,13 @@ Manager:AddRoom("CITY_GATE", "Main Gate", "")
 Manager:AddRoom("VICTORY", "Outside", "")
 
 -- Room Attachments --
-DUNG_CELL:AttachRoom("DUNG_SECRET_PASSAGE", "East")
+--DUNG_CELL:AttachRoom("DUNG_SECRET_PASSAGE", "East")
 --DUNG_CELL:AttachRoom("DUNG_HALLWAY", "North")
 --UNG_HALLWAY:AttachRoom("DUNG_CELL", "South")
 DUNG_HALLWAY:AttachRoom("DUNG_ENTRANCE", "North")
 DUNG_ENTRANCE:AttachRoom("DUNG_HALLWAY", "South")
 DUNG_ENTRANCE:AttachRoom("CASTLE", "East")
-DUNG_SECRET_PASSAGE:AttachRoom("DUNG_CELL", "West")
+--DUNG_SECRET_PASSAGE:AttachRoom("DUNG_CELL", "West")
 DUNG_SECRET_PASSAGE:AttachRoom("SEWER_SW", "East")
 
 CASTLE:AttachRoom("DUNG_ENTRANCE", "West")
@@ -69,7 +69,23 @@ VICTORY:AttachRoom("CITY_GATE", "South")
 Manager:SetCurrentRoom("DUNG_CELL")
 
 -- Room Descriptions --
-
+DUNG_CELL:SetDescription("")
+DUNG_HALLWAY:SetDescription("")
+DUNG_ENTRANCE:SetDescription("")
+DUNG_SECRET_PASSAGE:SetDescription("")
+CASTLE:SetDescription("")
+SEWER_NW:SetDescription("")
+SEWER_SW:SetDescription("")
+SEWER_SE:SetDescription("")
+SEWER_NE:SetDescription("")
+SEWER_HIDEOUT:SetDescription("")
+CITY_STREET:SetDescription("")
+CITY_STREET_NORTH:SetDescription("")
+CITY_MERCHANT:SetDescription("")
+CITY_GARDEN:SetDescription("")
+CITY_CLOTHING:SetDescription("")
+CITY_GATE:SetDescription("")
+VICTORY:SetDescription("")
 	
 -- Items for the Hobbit Hole
 Manager:CreateItem("DISGUISE", "Disguise", "A set of clothing designed to conceal your identity discreetly.", false)
@@ -78,10 +94,13 @@ Manager:CreateItem("CELL_DOOR",
 	"It looks very heavy and is locked with a fairly primitive locking mechanism.", true)
 Manager:CreateItem("CELL_KEYS",
 	"Keys", "Set of bronze keys that look like they could unlock a certain cell door.", false)
+Manager:CreateItem("CELL_BRICK",
+	"False Brick", "A peculiar looking brick that seems to be offset from the rest.  Perhaps I should press it?", true)
 
 CITY_CLOTHING:AddItem("DISGUISE")
 DUNG_CELL:AddItem("CELL_DOOR")
 DUNG_CELL:AddItem("CELL_KEYS")
+DUNG_CELL:AddItem("CELL_BRICK")
 DUNG_HALLWAY:AddItem("CELL_DOOR")
 
 --[[HH_LIVING_ROOM:AddItem("JOURNAL")
--- scripts/item_scripts.lua
+++ scripts/item_scripts.lua
@@ -4,10 +4,21 @@ CELL_DOOR_SCRIPTS = {
 			if Manager:PlayerHasItem("CELL_KEYS") then
 				DUNG_CELL:AttachRoom("DUNG_HALLWAY", "North")
 				DUNG_HALLWAY:AttachRoom("DUNG_CELL", "South")
-				Manager:Print("You unlocked the cell door!")
+				Manager:Print("You unlocked the cell door with the keys!")
 			else
 				Manager:Print("You have no way to unlock the cell door.")
 			end
+		end
+	end
+}
+
+CELL_BRICK_SCRIPTS = {
+	OnInteract = function(action)
+		if action == "press" then 
+			Manager:Print("You press the brick, pushing it inwards into the wall.  "..
+						  "A small doorway opens up hidden away revealing a secret passage!")
+			DUNG_CELL:AttachRoom("DUNG_SECRET_PASSAGE", "East")
+			DUNG_SECRET_PASSAGE:AttachRoom("DUNG_CELL", "West")
 		end
 	end
 }
