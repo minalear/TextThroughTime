a2f59d6103f67679da75fd92d6761219b365a545
Trevor Fisher <minalear@gmail.com>
2019-01-08 00:31:16 -0600

    Fixed tabs in main and added lore ideas.

--- src/main.cpp
+++ src/main.cpp
@@ -19,40 +19,40 @@ void callback(const std::string &str) {
 
 int main(int argc, char* argv[]) {
     minalear::GameWindow game_window("A Text Through Time", 800, 480);
-	
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	
-	glClearColor(0.f, 0.f, 0.f, 1.f);
 
-	window_manager = new WindowManager(&game_window);
-	window_manager->set_title("A Text Through Time");
-	window_manager->set_callback(callback);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	game_manager = new GameManager(window_manager);
-	
-	SDL_Event windowEvent;
-	while (true) {
-		if (SDL_PollEvent(&windowEvent)) {
-			// Break out of game loop
-			if (windowEvent.type == SDL_QUIT) break;
-			else if (windowEvent.type == SDL_TEXTINPUT) {
-			    window_manager->text_input(windowEvent.text.text[0]);
-			}
-			else if (windowEvent.type == SDL_KEYDOWN) {
-			    window_manager->keydown(windowEvent.key.keysym);
-			}
-		}
-		
-		glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.f, 0.f, 0.f, 1.f);
 
-		window_manager->render_to_screen();
+    window_manager = new WindowManager(&game_window);
+    window_manager->set_title("A Text Through Time");
+    window_manager->set_callback(callback);
 
-		game_window.dt();
-		game_window.swap_buffers();
-	} // End main game loop
+    game_manager = new GameManager(window_manager);
 
-	delete game_manager;
-	delete window_manager;
+    SDL_Event windowEvent;
+    while (true) {
+        if (SDL_PollEvent(&windowEvent)) {
+            // Break out of game loop
+            if (windowEvent.type == SDL_QUIT) break;
+            else if (windowEvent.type == SDL_TEXTINPUT) {
+                window_manager->text_input(windowEvent.text.text[0]);
+            }
+            else if (windowEvent.type == SDL_KEYDOWN) {
+                window_manager->keydown(windowEvent.key.keysym);
+            }
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        window_manager->render_to_screen();
+
+        game_window.dt();
+        game_window.swap_buffers();
+    } // End main game loop
+
+    delete game_manager;
+    delete window_manager;
     return 0;
 }
